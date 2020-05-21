3a776d805bab1646d9c300a5bff6329b6a87a4ef
Ros-ua <51153745+Ros-ua@users.noreply.github.com>
2020-05-21 14:57:12 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 sudo yum install cockpit firewalld -y
-&& sudo systemctl enable --now cockpit.socket
-&& sudo systemctl start firewalld
-&& sudo systemctl enable firewalld
-&& sudo systemctl status firewalld
-&& sudo firewall-cmd --permanent --zone=public --add-service=cockpit
-&& sudo firewall-cmd --reload
+sudo systemctl enable --now cockpit.socket
+sudo systemctl start firewalld
+sudo systemctl enable firewalld
+sudo systemctl status firewalld
+sudo firewall-cmd --permanent --zone=public --add-service=cockpit
+sudo firewall-cmd --reload
+yum update -y
+yum install wget ufw nano sudo curl -y
+sudo passwd root
