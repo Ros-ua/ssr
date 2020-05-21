0ccb06636c29c606d73363d17bdaac90af6acfc1
Ros-ua <51153745+Ros-ua@users.noreply.github.com>
2020-05-21 17:43:12 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,8 @@ sudo passwd root
 yum update -y && yum install wget ufw nano sudo curl -y
 wget https://raw.githubusercontent.com/FunctionClub/Fail2ban/master/fail2ban.sh && bash fail2ban.sh 2>&1 | tee fail2ban.log
 sudo systemctl restart sshd && fail2ban-client status ssh-iptables
-curl -O https://raw.githubusercontent.com/Ros-ua/ssr/master/trojan_mult.sh && chmod +x trojan_mult.sh && ./trojan_mult.sh
+curl -O https://raw.githubusercontent.com/atrandys/trojan/master/trojan_mult.sh && chmod +x trojan_mult.sh && ./trojan_mult.sh
+nano /usr/src/trojan/server.conf
+systemctl restart trojan
 source <(curl -sL https://multi.netlify.app/v2ray.sh)
 wget -N --no-check-certificate https://raw.githubusercontent.com/chiakge/Linux-NetSpeed/master/tcp.sh && chmod +x tcp.sh && ./tcp.sh
