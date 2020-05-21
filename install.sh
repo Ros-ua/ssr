3c861fe8e2fe489c4ce33c2e678f3fea822f4be6
Ros-ua <51153745+Ros-ua@users.noreply.github.com>
2020-05-21 16:29:34 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,5 +11,7 @@ sudo passwd root
 wget https://raw.githubusercontent.com/FunctionClub/Fail2ban/master/fail2ban.sh && bash fail2ban.sh 2>&1 | tee fail2ban.log
 sudo systemctl restart sshd && fail2ban-client status ssh-iptables
 curl -O https://raw.githubusercontent.com/Ros-ua/ssr/master/trojan_mult.sh && chmod +x trojan_mult.sh && ./trojan_mult.sh
+yum install https://repo.ius.io/ius-release-el7.rpm -y
+yum install python36u -y
 source <(curl -sL https://multi.netlify.app/v2ray.sh)
 wget -N --no-check-certificate https://raw.githubusercontent.com/chiakge/Linux-NetSpeed/master/tcp.sh && chmod +x tcp.sh && ./tcp.sh
