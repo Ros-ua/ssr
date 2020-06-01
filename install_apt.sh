6dcfc257f8089b0d108703e335d818a3b1af736f
Ros-ua <51153745+Ros-ua@users.noreply.github.com>
2020-06-01 19:45:50 +0800

    Update install_apt.sh

--- install_apt.sh
+++ install_apt.sh
@@ -5,8 +5,6 @@ sudo passwd root
 apt-get update &&  apt-get upgrade -y && apt-get install wget ufw nano sudo curl -y
 wget https://raw.githubusercontent.com/FunctionClub/Fail2ban/master/fail2ban.sh && bash fail2ban.sh 2>&1 | tee fail2ban.log
 sudo systemctl restart sshd && fail2ban-client status ssh-iptables
-curl -O https://raw.githubusercontent.com/Ros-ua/ssr/master/trojan_mult.sh && chmod +x trojan_mult.sh && ./trojan_mult.sh
-nano /usr/src/trojan/server.conf
-systemctl restart trojan
 source <(curl -sL https://multi.netlify.app/v2ray.sh)
 wget -N --no-check-certificate https://raw.githubusercontent.com/chiakge/Linux-NetSpeed/master/tcp.sh && chmod +x tcp.sh && ./tcp.sh
+apt-get update && apt-get install sudo curl -y && sudo bash -c "$(curl -fsSL https://raw.githubusercontent.com/johnrosen1/vpstoolbox/master/vps.sh)"
