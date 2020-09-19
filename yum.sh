9b82e1c86f0ca33236d391387b18464176c3ad2c
Ros-ua <51153745+Ros-ua@users.noreply.github.com>
2020-09-19 22:40:57 +0800

    Update yum.sh

--- yum.sh
+++ yum.sh
@@ -1,8 +1,5 @@
-sudo passwd root
 yum update -y && yum install wget ufw nano sudo curl -y
 wget -N --no-check-certificate https://raw.githubusercontent.com/chiakge/Linux-NetSpeed/master/tcp.sh && chmod +x tcp.sh && ./tcp.sh
-yum install firewalld -y && systemctl enable firewalld && systemctl start firewalld
-sudo yum install cockpit -y && sudo systemctl enable --now cockpit.socket && sudo firewall-cmd --permanent --zone=public --add-service=cockpit && sudo firewall-cmd --reload
 wget https://raw.githubusercontent.com/FunctionClub/Fail2ban/master/fail2ban.sh && bash fail2ban.sh 2>&1 | tee fail2ban.log
 sudo systemctl restart sshd && fail2ban-client status ssh-iptables
 curl -O https://raw.githubusercontent.com/atrandys/trojan/master/trojan_mult.sh && chmod +x trojan_mult.sh && ./trojan_mult.sh
